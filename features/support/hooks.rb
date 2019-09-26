387a2d6ea66c6e870c487c60b5222b47249457d9
Perrut <estevao.perrut@gmail.com>
2019-09-26 13:57:19 -0300

    adicionando screenshots

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -1,18 +1,17 @@
 Before do
-    @login_page = LoginPage.new
-    @movie_page = MoviePage.new
-    @sidebar = SideBarWiew.new
-    page.current_window.resize_to(1440, 900)
+  @login_page = LoginPage.new
+  @movie_page = MoviePage.new
+  @sidebar = SideBarWiew.new
+  page.current_window.resize_to(1440, 900)
 end
 
 Before ("@login") do
-    user = CONFIG["users"]["cat_manager"]
-    @login_page.go
-    @login_page.with(user["email"], user["pass"])
+  user = CONFIG["users"]["cat_manager"]
+  @login_page.go
+  @login_page.with(user["email"], user["pass"])
 end
 
-After do
- temp_shot = page.save_screenshot("log/temp_shot.png")
- screenshot = Base64.encode64(File.open(temp_shot).read)
- embed(screenshot, "image/png", "Screenshot")
+After do |scenario|
+  screenshot = page.save_screenshot("log/screenshots/#{scenario.__id__}.png")
+  embed(screenshot, "image/png", "Screenshot")
 end
